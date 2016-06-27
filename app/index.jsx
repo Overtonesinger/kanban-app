4a941276c0b2e2de2c3a5c8b2ab59400fc80a78d
Filip Rydlo <filip.rydlo@gmail.com>
2016-06-27 10:42:32 +0200

    added new components.

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//custom imports
+import App from './components/App';
 
 if(process.env.NODE_ENV !== 'production') {
   React.Perf = require('react-addons-perf');
 }
 
+//<div>Ahoj svete REACTu a webpacku! :D</div>,
 ReactDOM.render(
-	<div>Ahoj svete REACTu a webpacku! :D</div>,
-  document.getElementById('app')
+	<App />,
+	document.getElementById('app')
 );
-
