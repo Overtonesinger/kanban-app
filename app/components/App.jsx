5b2da9cd5da28bc67d112557a447bc672ebfff75
Filip Rydlo <filip.rydlo@gmail.com>
2016-06-30 15:24:05 +0200

    Alt: finished Connect adapter

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -4,108 +4,117 @@ var ExecutionEnvironment = require('exenv');
 import Base from './BaseComponent';  // this._bind() ....... the "mass-binder"
 import Notes from './Notes';
 
-export default class App extends Base {
-  constructor(props) {
-    super(props);
-    // this._bind('_handleClick', '_handleFoo');    //can NOW be used - instead of:
-      // this. _handleClick = this. _handleClick.bind(this);
-      // this. _handleFoo = this. _handleFoo.bind(this);
+import connect from '../libs/connect';
 
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn ES6'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
-  }
+class App extends Base {
+	constructor(props) {
+		super(props);
+		// this._bind('_handleClick', '_handleFoo');    //can NOW be used - instead of:
+			// this. _handleClick = this. _handleClick.bind(this);
+			// this. _handleFoo = this. _handleFoo.bind(this);
 
-  //----render
-  render() {
-    const {notes} = this.state;
+		this.state = {
+			notes: [
+				{
+					id: uuid.v4(),
+					task: 'Learn React'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Learn ES6'
+				},
+				{
+					id: uuid.v4(),
+					task: 'Do laundry'
+				}
+			]
+		};
+	}
 
-    return (
-      <div>
-        <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes notes={notes}
-          onNoteClick={this.activateNoteEdit}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote}
-          />
-      </div>
-    );
-  }
-  /* ------------wrong syntax-highlight STOPPER!------------- */
+	//----render
+	render() {
+		const {notes} = this.state;
 
-  //----add a note
-  addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'Novej task'
-      }])
-    });
-  }
+		return (
+			<div>
+				{this.props.test}
+				<button className="add-note" onClick={this.addNote}>+</button>
+				<Notes
+					notes={notes}
+					onNoteClick={this.activateNoteEdit}
+					onEdit={this.editNote}
+					onDelete={this.deleteNote}
+					/>
+			</div>
+		);
+	}
+	/* ------------wrong syntax-highlight STOPPER!------------- */
 
-  //----delete a note
-  deleteNote = (id, e) => {
-    e.stopPropagation();  // Avoid bubbling to edit
+	//----add a note
+	addNote = () => {
+		this.setState({
+			notes: this.state.notes.concat([{
+				id: uuid.v4(),
+				task: 'Novej task'
+			}])
+		});
+	}
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  }
+	//----delete a note
+	deleteNote = (id, e) => {
+		e.stopPropagation();  // Avoid bubbling to edit
 
-  //----begin editing
-  activateNoteEdit = (id) => {
-    this.setState({
-      notes: this.state.notes.map(note => {
-        if(note.id === id)
-        {
-          note.editing = true;
-        }
-        return note;
-      })
-    });
-  }
+		this.setState({
+			notes: this.state.notes.filter(note => note.id !== id)
+		});
+	}
 
-  editNote = (id, task) => {
-    this.setState({
-      notes: this.state.notes.map(note => {
-        if(note.id === id)
-        {
-          note.editing = false;
-          note.task = task;
-        }
-        return note;
-      })
-    });
-  }
+	//----begin editing
+	activateNoteEdit = (id) => {
+		this.setState({
+			notes: this.state.notes.map(note => {
+				if(note.id === id)
+				{
+					note.editing = true;
+				}
+				return note;
+			})
+		});
+	}
+
+	editNote = (id, task) => {
+		this.setState({
+			notes: this.state.notes.map(note => {
+				if(note.id === id)
+				{
+					note.editing = false;
+					note.task = task;
+				}
+				return note;
+			})
+		});
+	}
 
 /*
-  //----debug methods...
-  componentWillMount() {
-    if (ExecutionEnvironment.canUseViewport) {
-      console.log('componentWillMount() Triggered on CLIENT.');
-    }
-    else console.log('componentWillMount() now triggered on server-side.');
-  }
+	//----debug methods...
+	componentWillMount() {
+		if (ExecutionEnvironment.canUseViewport) {
+			console.log('componentWillMount() Triggered on CLIENT.');
+		}
+		else console.log('componentWillMount() now triggered on server-side.');
+	}
 
-  //----debug
-  componentDidMount() {
-    if (ExecutionEnvironment.canUseViewport) {
-      console.log('componentDidMount() Triggered on CLIENT.');
-    }
-    else console.log('componentDidMount() now triggered on server-side.');
-  }
+	//----debug
+	componentDidMount() {
+		if (ExecutionEnvironment.canUseViewport) {
+			console.log('componentDidMount() Triggered on CLIENT.');
+		}
+		else console.log('componentDidMount() now triggered on server-side.');
+	}
 */
 }
+
+
+export default connect(() => ({
+	test: 'test'
+}))(App)
